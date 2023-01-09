869bc1ebc28441503a3e38f9550211e31c88b628
sanskar06 <sanskar.chourasia333@gmail.com>
2023-01-09 22:28:44 +0530

    list and describe the topic

---
+++ topic-list.py
@@ -0,0 +1,10 @@
+from kafka import KafkaAdminClient
+client = KafkaAdminClient(bootstrap_servers='localhost:9092')
+topics = client.list_topics()
+for topic in topics:
+    print(topic)
+
+client = KafkaAdminClient(bootstrap_servers='localhost:9092')
+topic1 = client.describe_topics(['my-topic'])
+print(topic1)
+# topic1 will return the list of dict having error code , topic name and partition info 
