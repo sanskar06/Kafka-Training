b4e6c14614f3ad5b3617c0174aacff08440bc13b
sanskar06 <sanskar.chourasia333@gmail.com>
2023-01-10 15:23:30 +0530

    producer with serilizer

--- producer.py
+++ producer.py
@@ -1,13 +1,11 @@
 from kafka import  KafkaProducer
 import json
 
+
 def producer_with_keys():
     producer = KafkaProducer(bootstrap_servers='localhost:9092')
-
     producer.send('my-topic', key=b'key1', value=b'value1')
-
     producer.send('my-topic', key=b'key2', value=b'value2')
-
     producer.flush()
 
 def producer_with_value_serializer():
@@ -17,7 +15,7 @@ def producer_with_value_serializer():
 
 def producer_with_key_serializer():
     producer = KafkaProducer(key_serializer=str.encode)
-    producer.send('my-topic', key='ping', value=b'1234')
+    producer.send('my-topic', key='key', value=b'1234')
 
 producer_with_value_serializer()
 
